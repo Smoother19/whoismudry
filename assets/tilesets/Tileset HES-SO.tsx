4962ebf9424374de8b82aa67c1d607dbc1e95881
smooth <aftiteuf@gmail.com>
2026-05-09 20:46:32 +0200

    feat: Assets Loading

---
+++ assets/tilesets/Tileset HES-SO.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="Tileset HES-SO" tilewidth="32" tileheight="32" tilecount="20" columns="5">
+ <image source="tilesheet_finale.png" width="160" height="128"/>
+</tileset>
